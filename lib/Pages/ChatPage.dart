ae40642cce95c6a349ade73432fe2993e694420c
platinumpizza29 <kbelgi1@gmail.com>
2022-12-25 10:26:09 +0000

    Chat Page minor chat updates

--- lib/Pages/ChatPage.dart
+++ lib/Pages/ChatPage.dart
@@ -1,11 +1,20 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_typing_uninitialized_variables, unnecessary_null_comparison, prefer_if_null_operators
 
 import 'package:chat_bubbles/chat_bubbles.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:grpc/grpc.dart';
+import 'package:lottie/lottie.dart';
+
+import '../generated/message.pbgrpc.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key, required this.messages, required this.keys});
+  const ChatPage({
+    super.key,
+    required this.messages,
+    required this.keys,
+  });
   final List<String> messages;
   final keys;
 
@@ -14,6 +23,81 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  TextEditingController messageController = TextEditingController();
+  var sendMessage = "";
+  var isLoading = false;
+  var result;
+
+  Future<void> ChatService() async {
+    final channel = ClientChannel(
+      '18.133.75.244',
+      port: 8081,
+      options: ChannelOptions(
+        credentials: ChannelCredentials.insecure(),
+      ),
+    );
+    final stub = ChatMessageServiceClient(channel);
+
+    var input = ChatMessageRequest();
+    input.id = widget.keys;
+    input.message = messageController.text;
+    var lol = Stream.value(input);
+    try {
+      final response = stub.chat(lol);
+      await for (var v in response) {
+        setState(() {
+          var message1 = v.message;
+          widget.messages.add(message1);
+        });
+      }
+      // print('Greeter client received: ${response}');
+    } catch (e) {
+      print('Caught error: $e');
+    }
+    await channel.shutdown();
+  }
+
+  handleClick() {
+    if (widget.messages == null) {
+      return Center(
+        child: Lottie.asset('images/chatting.json',
+            height: MediaQuery.of(context).size.height / 2,
+            width: MediaQuery.of(context).size.width / 2),
+      );
+    } else {
+      return Column(
+        children: widget.keys
+            .map<Widget>((e) => Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  // child: SizedBox(
+                  //   height: 50,
+                  //   width: MediaQuery.of(context).size.width / 4,
+                  //   child: Card(
+                  //     child: ListTile(
+                  //       title: Text(e),
+                  //       leading: Icon(
+                  //         CupertinoIcons.person_alt_circle,
+                  //         color: Colors.deepPurpleAccent,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: BubbleSpecialOne(
+                      text: messageController.text,
+                      color: Color(0xFF1B97F3),
+                      tail: true,
+                      isSender: false,
+                      textStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ))
+            .toList(),
+      );
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -26,34 +110,61 @@ class _ChatPageState extends State<ChatPage> {
       appBar: CupertinoNavigationBar(
         middle: Text(widget.keys),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 500,
-            width: double.infinity,
-            color: Colors.teal,
-            child: ListView.builder(
-              itemCount: widget.messages == null ? 5 : widget.messages.length,
-              itemBuilder: (context, index) {
-                return BubbleSpecialThree(
-                  text: widget.messages[index].toString(),
-                  color: Color(0xFF1B97F3),
-                  tail: true,
-                  textStyle: TextStyle(color: Colors.white, fontSize: 16),
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: CupertinoTextField(
-                placeholder: "Type a message",
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.grey.shade400,
+              height: MediaQuery.of(context).size.height - 231,
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  itemCount:
+                      widget.messages == null ? 5 : widget.messages.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: BubbleSpecialOne(
+                        text: widget.messages[index].toString(),
+                        color: Color(0xFF1B97F3),
+                        tail: true,
+                        isSender: false,
+                        textStyle: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: CupertinoTextField(
+                  controller: messageController,
+                  keyboardType: TextInputType.text,
+                  placeholder: "Type a message",
+                  suffix: ButtonBar(
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.attach_file_rounded,
+                            color: Colors.deepPurpleAccent,
+                          )),
+                      IconButton(
+                          onPressed: () {
+                            ChatService();
+                            messageController.clear();
+                          },
+                          icon: Icon(
+                            Icons.send,
+                            color: Colors.deepPurpleAccent,
+                          )),
+                    ],
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/generated/message.pb.dart
@@ -0,0 +1,133 @@
+///
+//  Generated code. Do not modify.
+//  source: message.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class ChatMessageRequest extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'ChatMessageRequest', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'msgapi'), createEmptyInstance: create)
+    ..aOS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'id')
+    ..aOS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'message')
+    ..hasRequiredFields = false
+  ;
+
+  ChatMessageRequest._() : super();
+  factory ChatMessageRequest({
+    $core.String? id,
+    $core.String? message,
+  }) {
+    final _result = create();
+    if (id != null) {
+      _result.id = id;
+    }
+    if (message != null) {
+      _result.message = message;
+    }
+    return _result;
+  }
+  factory ChatMessageRequest.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory ChatMessageRequest.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  ChatMessageRequest clone() => ChatMessageRequest()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  ChatMessageRequest copyWith(void Function(ChatMessageRequest) updates) => super.copyWith((message) => updates(message as ChatMessageRequest)) as ChatMessageRequest; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static ChatMessageRequest create() => ChatMessageRequest._();
+  ChatMessageRequest createEmptyInstance() => create();
+  static $pb.PbList<ChatMessageRequest> createRepeated() => $pb.PbList<ChatMessageRequest>();
+  @$core.pragma('dart2js:noInline')
+  static ChatMessageRequest getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<ChatMessageRequest>(create);
+  static ChatMessageRequest? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get id => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set id($core.String v) { $_setString(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasId() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearId() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.String get message => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set message($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasMessage() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearMessage() => clearField(2);
+}
+
+class ChatMessageResponse extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'ChatMessageResponse', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'msgapi'), createEmptyInstance: create)
+    ..aOS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'id')
+    ..aOS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'message')
+    ..hasRequiredFields = false
+  ;
+
+  ChatMessageResponse._() : super();
+  factory ChatMessageResponse({
+    $core.String? id,
+    $core.String? message,
+  }) {
+    final _result = create();
+    if (id != null) {
+      _result.id = id;
+    }
+    if (message != null) {
+      _result.message = message;
+    }
+    return _result;
+  }
+  factory ChatMessageResponse.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory ChatMessageResponse.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  ChatMessageResponse clone() => ChatMessageResponse()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  ChatMessageResponse copyWith(void Function(ChatMessageResponse) updates) => super.copyWith((message) => updates(message as ChatMessageResponse)) as ChatMessageResponse; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static ChatMessageResponse create() => ChatMessageResponse._();
+  ChatMessageResponse createEmptyInstance() => create();
+  static $pb.PbList<ChatMessageResponse> createRepeated() => $pb.PbList<ChatMessageResponse>();
+  @$core.pragma('dart2js:noInline')
+  static ChatMessageResponse getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<ChatMessageResponse>(create);
+  static ChatMessageResponse? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get id => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set id($core.String v) { $_setString(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasId() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearId() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.String get message => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set message($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasMessage() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearMessage() => clearField(2);
+}
+
